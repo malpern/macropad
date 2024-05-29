cd7c7e84a3fad3f8457efbe8585fe15fe5cfcbfd
Micah Alpern <malpern@gmail.com>
2024-05-28 17:24:34 -0700

    hoping this has ecoder working for fast forward and rewind but not k. Still suspect encoder_handler.map is not needed

--- main.py
+++ main.py
@@ -50,7 +50,6 @@ def on_move_do(state):
 encoder_handler = EncoderHandler()
 encoder_handler.pins = ((keyboard.rgb_encoder_a, keyboard.rgb_encoder_b, None, False),)
 encoder_handler.on_move_do = lambda x, y, state: on_move_do(state)
-encoder_handler.map = [((KC.RGB_HUD, KC.RGB_HUI, [KC.RGB_TOG, KC.K]),),]  # Pressing the encoder button will toggle RGB and type K
 
 # Append extensions and modules
 keyboard.extensions.append(MediaKeys())
